22f98ade9dc7b7a384033bae221fe8c7b51a8aa1
= <aadipapsarponmisti@gmail.com>
2020-11-12 11:13:13 +0530

    Master c0036

---
+++ assets/sketchMob.js
@@ -0,0 +1,44 @@
+let mobilenet;
+let video;
+let label = '';
+let makers = ':Prediction(Probability):';
+
+function modelReady() {
+  console.log('Model is ready!!!');
+  mobilenet.predict(gotResults);
+}
+
+function gotResults(error, results) {
+  if (error) {
+    console.error(error);
+  } else {
+    //console.log(results);
+    prob = results[0].probability.toFixed(3);
+    label = results[0].className + "(Prob:" + prob + ")";
+    //label = results[0].label;
+    mobilenet.predict(gotResults);
+  }
+}
+
+// function imageReady() {
+//   image(puffin, 0, 0, width, height);
+// }
+
+function setup() {
+  var canvas = createCanvas(300, 260);
+  canvas.parent('sketch-holder');
+  video = createCapture(VIDEO);
+  video.size(300, 200);
+  video.hide();
+  background(0);
+  mobilenet = ml5.imageClassifier('MobileNet', video, modelReady);
+}
+
+function draw() {
+  background(255);
+  image(video, 0, 0);
+  fill(0);
+  textSize(10);
+  text(makers,80, height-20);
+  text(label, 10, height - 8);
+}
