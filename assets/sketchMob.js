dd01f1dc5857e60c176e603a5a626f4a15186ce2
= <aadipapsarponmisti@gmail.com>
2020-11-12 12:53:51 +0530

    Master c0042

--- assets/sketchMob.js
+++ assets/sketchMob.js
@@ -28,7 +28,7 @@ function setup() {
   var canvas = createCanvas(450, 400);
   canvas.parent('sketch-holder');
   video = createCapture(VIDEO);
-  video.size(500, 350);
+  video.size(450, 350);
   video.hide();
   background(0);
   mobilenet = ml5.imageClassifier('MobileNet', video, modelReady);
@@ -38,7 +38,7 @@ function draw() {
   background(255);
   image(video, 0, 0,450,350);
   fill(0);
-  textSize(10);
+  textSize(20);
   text(makers,180, height-20);
   text(label, 120, height - 8);
 }
